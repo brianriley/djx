325273db749a74ce6d0d91003cc2c8e8de71e4bd
Brian Riley <brian@btriley.com>
2010-11-06 22:50:00 -0700

    Added two new HTTP Response classes

--- README.rst
+++ README.rst
@@ -22,6 +22,18 @@ Example::
 	def some_view(request):
 		return HttpJSONResponse({'foo': 'bar'})
 
+HttpResponseCreated
+~~~~~~~~~~~~~~~~~~~
+
+HTTP 201 response. Should return the URI of the resource created.
+    
+See `HTTP/1.1 Status Code Definitions <http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.2.2>`_.
+
+HttpJSONResponseBadRequest
+~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+HTTP 400 response. Can return details of the errors with the request in the response body.
+
 decorators
 ==========
 
--- http.py
+++ http.py
@@ -18,3 +18,17 @@ class HttpJSONResponse(HttpResponse):
     """
     def __init__(self, content=''):
         super(HttpJSONResponse, self).__init__(content=json.dumps(content), mimetype='application/json')
+
+class HttpResponseCreated(HttpResponse):
+    """
+    HTTP 201 response. Should return the URI of the resource created.
+    
+    See `HTTP/1.1 Status Code Definitions <http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.2.2>`_.
+    """
+    status_code = 201
+
+class HttpJSONResponseBadRequest(HttpJSONResponse):
+    """
+    HTTP 400 response. Can return details of the errors with the request in the response body.
+    """
+    status_code = 400
